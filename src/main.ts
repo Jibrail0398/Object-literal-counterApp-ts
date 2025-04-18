3ce48a4859095467c8440accd71846b8be218e21
jibrail0398 <jibrailadji0398@gmail.com>
2025-04-18 18:48:47 +0700

    first-commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+type CounterInput = Array<string|number>|undefined;
+interface CounterOutput{
+    [key:string]:any
+}
+
+export function counterApp(data?: CounterInput): CounterOutput|string {
+    //write your answer here
+    
+    return "";
+
+}
---
+++ test/main.test.ts
@@ -0,0 +1,20 @@
+import { counterApp } from '../src/main';
+
+describe("counterApp", () => {
+    describe("when data is an empty array", () => {
+        it('returns Data not found', () => {
+            expect(counterApp([])).toMatch("Data not found");
+        });
+    });
+
+    describe("when data is undefined", () => {
+        it("returns Invalid input", () => {
+            expect(counterApp()).toMatch("Invalid input");
+        });
+    });
+
+    it('returns the number of names that appear', () => {
+        expect(counterApp(['Adit', 'Rekti', 'Fahri', 'Acil', 'Fahri', 'Adit', 'Rekti', 'Adit', 'Fahri', 'Fahri'])).toEqual({ Adit: 3, Rekti: 2, Fahri: 4, Acil: 1 });
+        expect(counterApp([5, 6, 7, 5, 5, 7, 6, 7, 7, 7, 5, 6])).toEqual({ '5': 4, '6': 3, '7': 5 });
+    });
+});
